111f05edf0e48263fe4a034f82d07da95b053172
Marie Dereuse <mdereuse@student.42.fr>
2024-01-27 15:52:13 +0100

    ex01

---
+++ ex01/intern.py
@@ -0,0 +1,40 @@
+DEFAULT_NAME = "My name? I’m nobody, an intern, I have no name."
+COFFEE_TALK = "This is the worst coffee you ever tasted."
+EXCEPTION_TXT = "I’m just an intern, I can’t do that..."
+
+
+class Intern:
+    class Coffee:
+        @staticmethod
+        def __str__():
+            return COFFEE_TALK
+
+    def __init__(self, name=DEFAULT_NAME):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def work():
+        raise Exception(EXCEPTION_TXT)
+
+    @classmethod
+    def make_coffee(cls):
+        return cls.Coffee()
+
+
+def main():
+    intern0 = Intern()
+    intern1 = Intern("Mark")
+    print(intern0)
+    print(intern1)
+    print(intern1.make_coffee())
+    try:
+        intern0.work()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
