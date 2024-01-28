5d7da3b5d0e76635f3bc07c4141ce44c9d0f1563
Marie Dereuse <mdereuse@student.42.fr>
2024-01-28 19:02:15 +0100

    ex06 finish

--- ex06/Page.py
+++ ex06/Page.py
@@ -76,14 +76,14 @@ class Page:
                 return False
         elif isinstance(elem, Text):
             return True
-        return all(Page._is_elem_valid(child) for child in node.content)
+        return all(Page._is_elem_valid(child) for child in elem.content)
 
     def is_valid(self):
         return Page._is_elem_valid(self.elem)
 
     def __str__(self):
         if isinstance(self.elem, Html):
-            return "<!DOCTYPE html>" + str(self.elem)
+            return "<!DOCTYPE html>\n" + str(self.elem)
         else:
             return str(self.elem)
 
@@ -93,7 +93,459 @@ class Page:
             
 
 def main():
-    print(all([]))
+    print("TEST VALID")
+    test_ok = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_ok.is_valid())
+    print()
+    print("Head is before Body")
+    test_html = Page(Html([
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ]),
+        Head(
+            Title(Text("\"Hello ground!\""))
+        )
+    ]))
+    print(test_html.is_valid())
+    print()
+    print("Two titles")
+    test_head = Page(Html([
+        Head([
+            Title(Text("\"Hello ground!\"")),
+            Title(Text("2nd Title"))
+        ]),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_head.is_valid())
+    print()
+    print("Wrong element type in body")
+    test_body = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Li(Text("test")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_body.is_valid())
+    print()
+    print("Title contains more than one text")
+    test_title = Page(Html([
+        Head(
+            Title([Text("\"Hello ground!\""), Text("pouet")])
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_title.is_valid())
+    print()
+    print("P contains a non text")
+    test_p = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P."),
+                        Li(Text("pouet"))
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_p.is_valid())
+    print()
+    print("Span contains a forbidden element")
+    test_span = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P."),
+                    ]),
+                    Text("This is a text outside a P."),
+                    Li(Text("pouet"))
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_span.is_valid())
+    print()
+    print("Ul contains no element")
+    test_ul = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_ul.is_valid())
+    print()
+    print("Ol contains other than Li")
+    test_ol = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+                Ol([
+                    Li(Text("truc")), Text("bidule"), Li(Text("chouette"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_ol.is_valid())
+    print()
+    print("Tr contains Td and Th")
+    test_tr = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Td(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ])
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_tr.is_valid())
+    print()
+    print("Table contains other than Tr")
+    test_table = Page(Html([
+        Head(
+            Title(Text("\"Hello ground!\""))
+        ),
+        Body([
+            H1(Text("\"Oh no, not again!\"")),
+            Div([
+                H2(Text("\"Yes, again.\"")),
+                Span([
+                    P([
+                        Text("This is one text in a P."),
+                        Text("This is a second text in a P.")
+                    ]),
+                    Text("This is a text outside a P.")
+                ])
+            ]),
+            Div([
+                Table([
+                    Tr([
+                        Th(Text("Pif")), Th(Text("Paf")), Th(Text("Pouf"))
+                    ]),
+                    Tr([
+                        Td(Text("1")), Td(Text("2")), Td(Text("3"))
+                    ]),
+                    Tr([
+                        Td(Text("4")), Td(Text("5")), Td(Text("6"))
+                    ]),
+                    Text("pouet")
+                ]),
+                Ul([
+                    Li(Text("truc")), Li(Text("bidule")), Li(Text("chouette"))
+                ]),
+                Ol([
+                    Li(Text("chose")), Li(Text("machin")), Li(Text("blop"))
+                ]),
+            ])
+        ])
+    ]))
+    print(test_table.is_valid())
+    print()
+    print()
+    print("DISPLAY")
+    print()
+    print(test_ok)
+    print()
+    print("print only a div :")
+    print()
+    print(Page(Div(Text("pouet"))))
+    test_ok.write_to_file("test.html")
 
 
 if __name__ == "__main__":
